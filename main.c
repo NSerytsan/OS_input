b3cb053db40f464c5cf50d145bdc40b064727d52
NSerytsan <nserytsan@gmail.com>
2023-04-20 17:10:49 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <linux/input.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int loop = 1;
+
+int main(int argc, char **argv)
+{
+    char *kb_input_device = "/dev/input/event4";
+    int kb_input_device_fd = -1;
+    struct input_event event;
+    size_t event_size = sizeof(struct input_event);
+    ssize_t bytes_read = 0;
+    int i = 0;
+
+    if ((kb_input_device_fd = open(kb_input_device, O_RDONLY)) == -1)
+    {
+        fprintf(stderr, "Couldn't open the device file <%s>.", kb_input_device);
+        return -1;
+    }
+
+    while (loop)
+    {
+        bytes_read = read(kb_input_device_fd, &event, event_size);
+        
+            if (event.type == EV_KEY)
+            {
+                if(event.value == 0)
+                {
+                    printf("%d", event.code);
+                }
+            }
+            fflush(stdout);
+        
+    }
+
+    close(kb_input_device_fd);
+    return 0;
+}
