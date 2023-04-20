278ac30d08b904142d8f8ba0ddf402a88d8e8f27
NSerytsan <nserytsan@gmail.com>
2023-04-21 00:24:02 +0300

    clean code

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 
 int loop = 1;
 
-
 void sig_handler()
 {
     loop = 0;
@@ -17,12 +16,9 @@ int main(int argc, char **argv)
     char *kb_input_device = "/dev/input/event4";
     int kb_input_device_fd = -1;
     struct input_event event;
-    size_t event_size = sizeof(struct input_event);
-    ssize_t bytes_read = 0;
-    int i = 0;
 
     signal(SIGINT, sig_handler);
-    
+
     if ((kb_input_device_fd = open(kb_input_device, O_RDONLY)) == -1)
     {
         fprintf(stderr, "Couldn't open the device file <%s>.", kb_input_device);
@@ -31,17 +27,16 @@ int main(int argc, char **argv)
 
     while (loop)
     {
-        bytes_read = read(kb_input_device_fd, &event, event_size);
-        
-            if (event.type == EV_KEY)
+        read(kb_input_device_fd, &event, sizeof(struct input_event));
+
+        if (event.type == EV_KEY)
+        {
+            if (event.value == 0)
             {
-                if(event.value == 0)
-                {
-                    printf("%d", event.code);
-                }
+                printf("%d", event.code);
             }
-            fflush(stdout);
-        
+        }
+        fflush(stdout);
     }
 
     close(kb_input_device_fd);
